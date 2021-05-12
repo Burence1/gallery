961e3d437f58b3cd7aeb0b99e5cc9cf63f09701f
Burence1 <burensomondi@gmail.com>
2021-05-12 15:29:23 +0300

    add db schema

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+class Category(models.Model):
+  name=models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
+
+class Location(models.Model):
+  name=models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
+
+class Images(models.Model):
+  name=models.CharField(max_length=50)
+  image=
