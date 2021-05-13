791e55a586de0c66b5804838d75f067a93404d37
Burence1 <burensomondi@gmail.com>
2021-05-13 13:08:19 +0300

    add search logic

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
   path('',views.index,name='home'),
+  path('search/',views.search_images,name='search_results')
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,4 @@
+from django.core.checks import messages
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 from .models import Location,Category,Images
@@ -5,11 +6,26 @@ from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def index(request):
-  images = Images.objects.all()
+  images = Images.get_images()
   location = Location.objects.all()
   category= Category.objects.all()
 
   return render(request, 'index.html',{"images":images,"location":location,"category":category})
 
 def search_images(request):
-  
+  if 'category' in request.GET and request.GET["category"]:
+    search_term=request.GET.get("category")
+    try:
+      category=Category.objects.get(name=search_term)
+      image=Images.search_image(category)
+      message=f"found {{images|length}}"
+      return render(request,'search.html',{"message":message,"image":image})
+    
+    except ObjectDoesNotExist:
+      message="O images found"
+      categories= Category.objects.all()
+      return render(request,"search.html",{"message":message,"categories":categories})
+
+  else:
+    message="Search for categories"
+    return render(request,"search.html",{"message":message})
