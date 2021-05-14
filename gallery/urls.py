cab33a9ba71e280f0ede96fce23599f28fe661c3
Burence1 <burensomondi@gmail.com>
2021-05-14 10:23:07 +0300

    fix url paths

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path,re_path
 from .import views
 from django.conf.urls.static import static
 
@@ -8,10 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
   path('',views.index,name='home'),
   path('search/',views.search_images,name='search_results'),
-  url(r'^category/(?P<category_id>\d+)', views.category, name='category'),
-  url(r'^location/(?P<location_id>\d+)', views.location, name='location'),
-  # path('location/(P<location_id>\d+)',views.location, name='location'),
-  # path('category/(P<category_id>\d+)', views.category, name='category')
+  re_path('location/(?P<location_id>\d+)', views.location, name='location'),
+  re_path('category/(?P<category_id>\d+)', views.category, name='category')
 ]
 
 if settings.DEBUG:
